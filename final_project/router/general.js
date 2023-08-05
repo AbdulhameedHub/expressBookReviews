647a084010e610718839ac8bfffd93c88d72a9b7
AbdulhameedHub <127307176+AbdulhameedHub@users.noreply.github.com>
2023-08-05 15:16:51 +0100

    Updated

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,36 +6,58 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+      
+        if (username && password) {
+          if (!doesExist(username)) {
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registred. Now you can login"});
+          } else {
+            return res.status(404).json({message: "User already exists!"});
+          }
+        }
+        return res.status(404).json({message: "Unable to register user."});
+      });
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
+    res.send(JSON.stringify(books,null,4));
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
+    const ISBN = req.params.ISBN;
+    res.send(books[ISBN])
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    const author = req.query.author;
+    let filtered_author = books.filter((books)= books.author === author);
+    res.send(filtered_author);
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+    const title = req.query.title;
+    let filtered_title = books.filter((books) = books.title === title);
+    res.send(filtered_title);
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+    const review= req.params.review;
+    res.send(reviews[ISBN]);
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
